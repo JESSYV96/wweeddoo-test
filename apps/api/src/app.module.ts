d5f8e14a59ddb821bb4bdcee23eabc66f13a2527
jessyv96 <c.j91350@gmail.com>
2021-04-29 16:02:16 +0200

    Fix db in prod

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -27,6 +27,10 @@ import { MailModule } from './mail/mail.module';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
+      ssl:(process.env.NODE_ENV === 'production') && {
+        rejectUnauthorized: false
+      },
+      url: (process.env.NODE_ENV === 'production') && process.env.DATABASE_URL, 
       entities: [User, Project, Skill],
       synchronize: true,
     }),
